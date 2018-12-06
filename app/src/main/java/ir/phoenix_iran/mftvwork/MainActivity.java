cc39b18b3408cbbf8811f1261a400e774e051ab3
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-06 23:36:58 +0330

    commit init
    commit init

--- app/src/main/java/ir/phoenix_iran/mftvwork/MainActivity.java
+++ app/src/main/java/ir/phoenix_iran/mftvwork/MainActivity.java
@@ -1,14 +1,43 @@
 package ir.phoenix_iran.mftvwork;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton firstExercise , secondExercise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        firstExercise = findViewById(R.id.firstExercise);
+        secondExercise = findViewById(R.id.secondExercise);
+
+        firstExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIntents(FirstExerciseActivity.class);
+            }
+        });
+
+        secondExercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setIntents(SecondExerciseActivity.class);
+            }
+        });
+
+
     }
+
+    private void setIntents(Class<? extends AppCompatActivity> activity){
+        Intent intent = new Intent(this , activity.getClass());
+        startActivity(intent);
+    }
+
 }
