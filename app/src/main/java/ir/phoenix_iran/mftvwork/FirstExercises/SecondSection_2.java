909d47e259ea74716ec8c73f724cda495d939f53
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-07 00:31:53 +0330

    commit init
    commit init

--- app/src/main/java/ir/phoenix_iran/mftvwork/FirstExercises/SecondSection_2.java
+++ app/src/main/java/ir/phoenix_iran/mftvwork/FirstExercises/SecondSection_2.java
@@ -1,7 +1,9 @@
 package ir.phoenix_iran.mftvwork.FirstExercises;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ir.phoenix_iran.mftvwork.R;
 
@@ -11,5 +13,22 @@ public class SecondSection_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_section_2);
+
+        Intent intent = getIntent();
+        String firstName = intent.getStringExtra("firstName");
+        String lastName  = intent.getStringExtra("lastName");
+        String age       = intent.getStringExtra("age");
+        String phone     = intent.getStringExtra("phone");
+
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtLastName  = findViewById(R.id.txtLastName);
+        TextView txtAge       = findViewById(R.id.txtAge);
+        TextView txtPhone     = findViewById(R.id.txtPhone);
+
+        txtFirstName.setText(firstName);
+        txtLastName.setText(lastName);
+        txtAge.setText(age);
+        txtPhone.setText(phone);
+
     }
 }
