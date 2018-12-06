d1de546b1fc183aa57625dabb12f04c581f85ed1
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-07 01:20:04 +0330

    commit init
    commit init

--- app/src/main/java/ir/phoenix_iran/mftvwork/FirstExercises/FirstSection.java
+++ app/src/main/java/ir/phoenix_iran/mftvwork/FirstExercises/FirstSection.java
@@ -2,6 +2,7 @@ package ir.phoenix_iran.mftvwork.FirstExercises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import ir.phoenix_iran.mftvwork.R;
 
@@ -11,5 +12,15 @@ public class FirstSection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_section);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_first_section, menu);
+        return true;
+    }
+
 }
