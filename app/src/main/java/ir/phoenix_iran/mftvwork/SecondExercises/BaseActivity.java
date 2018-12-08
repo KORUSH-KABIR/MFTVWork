d81a211d310fee4701f48e8c6bfe0f2528bc603d
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-09 00:50:55 +0330

    commit init
    commit init

--- app/src/main/java/ir/phoenix_iran/mftvwork/SecondExercises/BaseActivity.java
+++ app/src/main/java/ir/phoenix_iran/mftvwork/SecondExercises/BaseActivity.java
@@ -1,6 +1,9 @@
 package ir.phoenix_iran.mftvwork.SecondExercises;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import ir.phoenix_iran.mftvwork.Core.SetupActivity;
 import ir.phoenix_iran.mftvwork.R;
@@ -11,5 +14,6 @@ public class BaseActivity extends SetupActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
     }
 }
