a35deca57725d96c44ec3eb4a941cce0ca4801e5
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-09 18:23:17 +0330

    commit init
    commit init

--- app/src/main/java/ir/phoenix_iran/mftvwork/SecondExercises/ProfileActivity.java
+++ app/src/main/java/ir/phoenix_iran/mftvwork/SecondExercises/ProfileActivity.java
@@ -3,14 +3,36 @@ package ir.phoenix_iran.mftvwork.SecondExercises;
 import android.os.Bundle;
 
 import ir.phoenix_iran.mftvwork.Core.SetupActivity;
+import ir.phoenix_iran.mftvwork.Core.UserHelper;
 import ir.phoenix_iran.mftvwork.R;
 
 public class ProfileActivity extends SetupActivity {
+
+    private UserHelper helper;
+    private String name , username , email , password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        helper = new UserHelper(this);
+        getDetail();
+
+
+
+    }
+
+    private void initViews(){
+
+
+
+    }
+
+    private void getDetail(){
+        name     = helper.getName();
+        username = helper.getUsername();
+        email    = helper.getEmail();
+        password = helper.getPassword();
     }
 
     @Override
