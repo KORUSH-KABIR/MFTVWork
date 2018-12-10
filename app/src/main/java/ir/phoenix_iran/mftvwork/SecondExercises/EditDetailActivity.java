b7fce4cb981a457837cf817bf09d05cb1e8eeda4
Mohammad Ali Riazati <mohammadali.riazati@yahoo.com>
2018-12-10 23:38:42 +0330

    commit init
    commit init

---
+++ app/src/main/java/ir/phoenix_iran/mftvwork/SecondExercises/EditDetailActivity.java
@@ -0,0 +1,15 @@
+package ir.phoenix_iran.mftvwork.SecondExercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ir.phoenix_iran.mftvwork.R;
+
+public class EditDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_detail);
+    }
+}
